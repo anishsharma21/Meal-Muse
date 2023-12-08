73b12954f9a82f8c116962abbce1da725c86c652
Anish Sharma <anisharma2102@gmail.com>
2023-12-09 00:53:59 +1100

    Initial Commit

---
+++ Meal Muse/Meal_MuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_MuseApp.swift
+//  Meal Muse
+//
+//  Created by Anish Sharma on 9/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Meal_MuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
