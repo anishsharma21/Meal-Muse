7b130c26b63c5f4f21606c21cae443a3ad30d87c
Anish Sharma <anisharma2102@gmail.com>
2023-12-12 01:00:29 +1100

    Added files for recipes (for real)

--- Meal Muse/Views/GeneratedMealsView.swift
+++ Meal Muse/Views/GeneratedMealsView.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import SwiftUI
+
+struct GeneratedMealsView: View {
+    @State private var meals = [String]()
+
+    var body: some View {
+        NavigationView {
+            List(meals, id: \.self) { meal in
+                Text(meal)
+            }
+            .navigationBarTitle("Meals")
+            .onAppear(perform: loadMeals)
+        }
+    }
+
+    func loadMeals() {
+        if let fileURL = Bundle.main.url(forResource: "meals", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileURL) {
+                meals = fileContents.components(separatedBy: "\n").filter { !$0.isEmpty }
+            }
+        }
+    }
+}
+
+struct MealListView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeneratedMealsView()
+    }
+}
