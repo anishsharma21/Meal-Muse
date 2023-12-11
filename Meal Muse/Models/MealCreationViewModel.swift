a905e1db34a52f855cca61d28305cfbb13013ff7
Anish Sharma <anisharma2102@gmail.com>
2023-12-11 23:51:53 +1100

    Added some more questions to the meal creation attributes

--- Meal Muse/Models/MealCreationViewModel.swift
+++ Meal Muse/Models/MealCreationViewModel.swift
@@ -7,14 +7,23 @@ class MealCreationViewModel: ObservableObject {
     @Published var currentQuestionIndex: Int = 0
 
     let questions: [Question] = [
-        Question(title: "What type of diet are you adhering to?", options: ["Vegan", "Vegetarian", "Anything", "Pescetarian"]),
-        Question(title: "What are your macro nutrient requirements?", options: ["High Protein", "High Fat", "High Carbs", "Low Carbs", "Low Protein", "Low Fat"]),
-        // ... more questions
+        Question(title: "What type of diet are you adhering to?", options: ["Vegan", "Vegetarian", "Carnivore", "Pescetarian", "Paleo", "Keto", "Mediterranean", "No preference"]),
+        Question(title: "What are your macro nutrient requirements?", options: ["High Protein", "High Carbs", "High Fat", "Balanced", "Low Carb", "Low Fat", "Low Protein"]),
+        Question(title: "How many calories should this meal have?", options: ["Under 100", "100 - 250", "250 - 500", "500 - 750", "750 - 1000", "1000+"]),
+        Question(title: "Preferred meal time", options: ["Breakfast", "Lunch", "Snack", "Dinner", "Dessert"]),
+        Question(title: "Desired cooking time", options: ["Under 15 minutes", "15-30 minutes", "30-45 minutes", "45-60 minutes", "Over an hour"])
+        // ... add more questions as needed
     ]
 
     func toggleOptionSelection(questionIndex: Int, option: String) {
-        userPreferences[questionIndex] = [option] // Replace the set with just this option
+        var selections = userPreferences[questionIndex] ?? Set<String>()
+        if selections.contains(option) {
+            selections.remove(option)
+        } else {
+            selections.insert(option)
+        }
+        userPreferences[questionIndex] = selections
     }
-
 }
+
 
